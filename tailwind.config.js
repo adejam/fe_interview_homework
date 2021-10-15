cc39dc8ad96209cce0c3a3eb0f95e9a01a4c6091
Adeleye Jamiu <oluwaloseyifunmiadeleye1@gmail.com>
2021-10-15 02:50:10 +0100

    chore: added safelist option on tailwind config as classes was being purged on production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  purge: ["./index.html", ".src/**/*.js"],
+  purge: {
+    content: ["./index.html", ".src/**/*.js"],
+    safelist: [  // added this safelist as the classes on js are not working on production(possibly being purged out during production builds). see https://tailwindcss.com/docs/optimizing-for-production#safelisting-specific-classes
+      "p-2",
+      "bg-indigo-500",
+      "text-white",
+      "rounded-md",
+      "cursor-pointer",
+      "text-center"
+    ] 
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
